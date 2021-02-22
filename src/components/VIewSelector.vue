0042fd781f16657f1a7322e5f22ed69467a3df66
LiamWu <lovan0616@gmail.com>
2021-02-22 11:40:04 +0800

    feat: add ViewSelector

---
+++ src/components/VIewSelector.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="view-selector">
+    <select v-model="selected">
+      <option v-for="view in views" :key="view.name" :value="view.name" :selected="view.name === routeName">{{ view.name_ch }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewSelector',
+  data() {
+    return {
+      selected: this.$route.name,
+      views: [{
+        name: 'calendar-day',
+        name_ch: '日檢視'
+      },{
+        name: 'calendar-week',
+        name_ch: '週檢視'
+      }],
+      routeName: this.$route.name
+    }
+  },
+  watch: {
+    selected(newValue, oldValue) {
+      console.log({
+        'newValue': newValue,
+        'oldValue': oldValue
+      })
+      const { date } = this.$route.params
+      this.$router.push({ name: newValue, date })
+    }
+  }
+}
+</script>
