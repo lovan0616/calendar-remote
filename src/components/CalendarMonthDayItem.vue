cf2e9d722f08da69fb8bdf809488bb0d44693e0e
LiamWu <lovan0616@gmail.com>
2021-03-04 00:51:10 +0800

    feat: add haveEvent design

--- src/components/CalendarMonthDayItem.vue
+++ src/components/CalendarMonthDayItem.vue
@@ -1,13 +1,14 @@
 <template>
   <ol class="days-grid d-flex flex-wrap">
     <li
+      v-for="day in days"
+      :key="day.date"
       class="calendar-day"
       :class="{
       'calendar-day--not-current':!day.isCurrentMonth,
-      'calendar-day-today':isToday
+      'calendar-day-today':isToday,
+      'have-event':haveEventDate.some(date => date === day.date)
     }"
-      v-for="day in days"
-      :key="day.date"
     >
       <router-link :to="{
       name: 'calendar-day',
@@ -121,6 +122,12 @@ export default {
         ...this.currentMonthDays,
         ...this.nextMonthDays
       ];
+    },
+    haveEventDate() {
+      const storageData = JSON.parse(localStorage.getItem("schedule"));
+      return storageData.map(item => {
+        return item.date;
+      });
     }
   },
   methods: {
@@ -141,18 +148,28 @@ a {
 ol {
   @include box-shadow;
   margin-bottom: 25px;
+  padding: 10px;
 }
 
 .calendar-day {
-    list-style: none;
-    width: calc(100% / 7);
-    text-align: center;
-    padding: 10px 0px 10px 0px;
-    color: #838383;
-  }
+  list-style: none;
+  width: calc(100% / 7);
+  text-align: center;
+  padding: 10px 0px 10px 0px;
+  color: #838383;
+}
 
 .calendar-day--not-current {
   color: #d3d3d3;
+}
+
+.have-event {
+  @include box-shadow;
+  background-color: $color_choice_1;
+  filter: opacity(40%);
+  color: $font_dark;
+  border-radius: 5px;
+  border: 1px solid #ffffff;
 }
 
 .date-item:hover {
